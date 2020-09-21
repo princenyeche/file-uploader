3677fcf3a30c2930e9cea82894dfb5eca827e415
Prince <hakwumerunyeche@gmail.com>
2020-09-21 18:31:59 +0800

    Create __init__.py

---
+++ uploader/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+Script   : Image Upload Service
+Author   : Prince Nyeche
+Platform : Web
+Version  : 0.1
+Function : Script helps to upload single image or multiple image files in a
+           zip folder
+**************************************************************************
+Required libraries : Flask
+Download URL       : pip install -r requirements.txt
+License            : MIT License Copyright (c) 2020 Prince Nyeche
+**************************************************************************
+"""
